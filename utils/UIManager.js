c4107cf0b0a8670e8085dbb511ac891311d4805f
GabrielxKuek <your-github-email@example.com>
2023-11-24 15:59:27 +0800

    created controls menu

--- utils/UIManager.js
+++ utils/UIManager.js
@@ -85,7 +85,51 @@ class UIManager {
             area(),
             anchor("center"),
             pos(center().x, center().y - 100)
-        ])
+        ]);
+
+        const controlPrompts = add([
+            pos(center().x + 30, center().y),
+        ]);
+
+        // child of controlPrompts
+        controlPrompts.add([
+            sprite("up"),
+            pos(0, -80),
+        ]);
+        controlPrompts.add([
+            sprite("down")
+        ]);
+        controlPrompts.add([
+            sprite("left"),
+            pos(-80, 0),
+        ]);
+        controlPrompts.add([
+            sprite("right"),
+            pos(80, 0),
+        ]);
+        controlPrompts.add([
+            sprite("space"),
+            pos(-200, 0),
+        ]);
+
+        controlPrompts.add([
+            text("Jump", { font: "Round", size: 32 }),
+            pos(-190, 100),
+        ]);
+        controlPrompts.add([
+            text("Move", { font: "Round", size: 32 }),
+            pos(10, 100),
+        ]);
+
+        this.displayBlinkingUIMessage(
+            "Press [ Enter ] to Start Game",
+            vec2(center().x, center().y + 300)
+        );
+
+        onKeyPress("enter", () => {
+            play("confirm-ui", { speed: 1.5 })
+            go("1")
+        })
     }
 }
 
